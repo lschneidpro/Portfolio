e5d0786d311dbbac13039afc88d56e68e5d3215a
Luca Schneider <lschneidpro@hmail.com>
2020-09-07 19:13:32 +0200

    bert

---
+++ SentimentAnalysis_webapp/distilbert-heroku/webapp/python/nlp.py
@@ -0,0 +1,19 @@
+from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification
+import torch.nn.functional as F
+import torch
+
+
+def get_probability(text):
+    
+    inputs = tokenizer(text, return_tensors="pt",truncation=True, padding=True, max_length=512)
+    with torch.no_grad():
+        outputs = model.forward(**inputs, return_dict=True)
+    probabilities = (F.softmax(outputs.logits, dim=1))
+    
+    prob_positive = probabilities.data.numpy()[0][1]
+    
+    return prob_positive
+
+model = DistilBertForSequenceClassification.from_pretrained('lschneidpro/distilbert_uncased_imdb')
+tokenizer = DistilBertTokenizerFast.from_pretrained('lschneidpro/distilbert_uncased_imdb')
+
